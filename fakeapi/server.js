01ac7944a2a08a91f57175ed8b5eee7158358e76
joswaita <waitajoseph@gmail.com>
2024-07-17 23:55:30 +0300

    registering multiple instances

--- fakeapi/server.js
+++ fakeapi/server.js
@@ -1,9 +1,10 @@
 import express from "express";
 import dotenv from 'dotenv'
+import axios from 'axios'
 dotenv.config()
 
-
-const PORT = process.env.PORT || 3001
+const HOST = 'localhost'
+const PORT = process.env.PORT || 3004
 const app = express();
 app.use(express.json())
 app.get('/fakeapi', (req, res, next) => {
@@ -15,5 +16,23 @@ app.get('/bogusApi', (req, res, next) => {
 })
 
 app.listen(PORT, () => {
+    axios(
+        {
+            method: 'POST',
+            url: 'http://localhost:3000/register',
+            data: {
+                apiName: "fakeApi1",
+                protocol: "http",
+                host: HOST,
+                port: PORT,
+
+            },
+            headers: { 'Content-Type': 'application/json' }
+        }
+    ).then((response) => {
+        console.log(response.data)
+    }).catch(error => {
+        console.log('Error:', error);
+    });
     console.log(`Server is running on port ${PORT}`)
 })
--- routes/index.js
+++ routes/index.js
@@ -1,39 +1,66 @@
 import express from "express";
 import axios from "axios";
 import registry from './registry.json' assert { type: 'json' };
-import fs from 'fs'
+import fs from 'fs';
 
 const router = express.Router();
-router.all('/:apiName/:path', (req, res) => {
-    //route request to api
-    console.log(req.params.apiName)
-    if (registry.services[req.params.apiName]) {
-        axios(
-            {
-                method: req.method,
-                url: registry.services[req.params.apiName].url + req.params.path,
-                data: req.body,
-                headers: req.headers
-            }
-        ).then((response) => {
-            res.send(response.data)
-        })
-    } else {
-        res.send("API not found")
-    }
-})
 
-//register server endpoint
+router.all('/:apiName/:path', (req, res) => {
+    // Route request to API
+    console.log(req.params.apiName);
+    if (registry.services[req.params.apiName]) {
+        axios({
+            method: req.method,
+            url: registry.services[req.params.apiName].url + req.params.path,
+            data: req.body,
+            headers: req.headers
+        }).then((response) => {
+            res.send(response.data);
+        }).catch((error) => {
+            res.status(500).send(error.message);
+        });
+    } else {
+        res.status(404).send("API not found");
+    }
+});
+
+// Register server endpoint
 router.post('/register', (req, res) => {
     const registrationInfo = req.body;
-    registry.services[registrationInfo.apiName] = { ...registrationInfo }
-    fs.writeFile('./routes/registry.json', JSON.stringify(registry), (err) => {
-        if (err) {
-            res.send("Could not register" + registrationInfo.apiName + '\n' + err)
-        } else {
-            res.send("Successfully registered" + registrationInfo.apiName)
+    registrationInfo.url = req.protocol + '://' + registrationInfo.host + ':' + registrationInfo.port + '/';
+    if (apiAlreadyExists(registrationInfo)) {
+        // Return already exists
+        res.send("Configuration already exists for " + registrationInfo.apiName + ' at ' + registrationInfo.host + ':' + registrationInfo.port);
+    } else {
+        // Register
+        if (!registry.services[registrationInfo.apiName]) {
+            registry.services[registrationInfo.apiName] = [];
         }
-    })
-})
 
-export default router
+        registry.services[registrationInfo.apiName].push({ ...registrationInfo });
+        fs.writeFile('./routes/registry.json', JSON.stringify(registry, null, 2), (err) => {
+            if (err) {
+                res.status(500).send("Could not register " + registrationInfo.apiName + '\n' + err.message);
+            } else {
+                res.send("Successfully registered " + registrationInfo.apiName);
+            }
+        });
+    }
+});
+
+const apiAlreadyExists = (registrationInfo) => {
+    if (!registry.services[registrationInfo.apiName]) {
+        return false;
+    }
+
+    let exists = false;
+    registry.services[registrationInfo.apiName].forEach((service) => {
+        if (service.url === registrationInfo.url) {
+            exists = true;
+            return;
+        }
+    });
+    return exists;
+};
+
+export default router;
