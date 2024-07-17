afbbb93d8e2fd62fed95b95ee524b9caf0f3bdad
joswaita <waitajoseph@gmail.com>
2024-07-18 00:43:30 +0300

    add helmet for security

--- gateway.js
+++ gateway.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import routes from './routes/index.js'
+import helmet from 'helmet';
 
 dotenv.config()
 
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 3000
 
 const app = express();
 app.use(express.json())
+app.use(helmet())
 
 app.use('/', routes)
 app.listen(PORT, () => {
